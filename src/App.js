c125a020a74a5f785114d16d6efd06bb3336f726
Elizabeth Villalejos <nekobanpaia@gmail.com>
2020-08-24 09:19:04 -0700

    Adding basic file structure, login page draft

--- src/App.js
+++ src/App.js
@@ -1,30 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {
+  Button,
+} from 'semantic-ui-react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    <div className="test">This is a test!</div>
+    <Button> Am I working now? </Button>
+  </div>
+);
 
 export default App;
---
+++ src/pages/login.jsx
@@ -0,0 +1,75 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import {
+  Button, Form, Grid, Header, Segment, Icon,
+} from 'semantic-ui-react';
+import userActions from '../redux/userActions';
+
+const LoginPage = props => {
+  const dispatch = useDispatch();
+  const [loginForm, setLoginForm] = useState({
+    email: '',
+    password: '',
+  });
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    const { history } = props;
+    dispatch(userActions.loginUser(loginForm));
+    history.push('/');
+  };
+
+  const handleChange = e => setLoginForm({ ...loginForm, [e.target.name]: e.target.value });
+
+  const { email, password } = loginForm;
+
+  return (
+    <Grid textAlign="center" className="middle aligned wall login">
+      <Grid.Column className="login-column">
+        <Header as="h2" className="login-header" textAlign="center">
+          <Icon name="home" />
+          Log-in to your account
+        </Header>
+        <Form size="large" onSubmit={handleSubmit}>
+          <Segment stacked>
+            <Form.Input
+              id="form-input-control-email"
+              fluid
+              icon="user"
+              iconPosition="left"
+              type="text"
+              name="email"
+              value={email}
+              onChange={handleChange}
+              placeholder="Email"
+              label="Email"
+              className="login-input"
+            />
+            <Form.Input
+              id="form-input-control-password"
+              fluid
+              icon="lock"
+              iconPosition="left"
+              type="password"
+              name="password"
+              value={password}
+              onChange={handleChange}
+              placeholder="Password"
+              label="Password"
+              className="login-input"
+            />
+            <Button className="login-button" fluid size="large" type="submit">
+              Login
+            </Button>
+          </Segment>
+        </Form>
+      </Grid.Column>
+    </Grid>
+  );
+};
+
+LoginPage.propTypes = {
+  history: PropTypes.oneOfType([PropTypes.object]).isRequired,
+};
+export default LoginPage;
