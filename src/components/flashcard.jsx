5ca33a241713ea6d64b29667da9459e1606782bc
Elizabeth Villalejos <nekobanpaia@gmail.com>
2020-08-24 15:22:51 -0700

    Adding yes/no buttons to flashcard, restyling sizes

--- src/components/flashcard.jsx
+++ src/components/flashcard.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect } from 'react';
-import { Card } from 'semantic-ui-react';
+import {
+  Card, Segment, Button, Icon, Container,
+} from 'semantic-ui-react';
 import './flashcard.css';
 
 const Flashcard = () => {
@@ -24,22 +26,34 @@ const Flashcard = () => {
   }, []);
 
   return (
-    <div id="card" className="flip-container">
-      <Card id="card-front" className="flip-card-front on">
-        <Card.Content>
-          <Card.Header>
-            Question
-          </Card.Header>
-        </Card.Content>
-      </Card>
-      <Card id="card-back" className="flip-card-back off">
-        <Card.Content>
-          <Card.Header>
-            Answer
-          </Card.Header>
-        </Card.Content>
-      </Card>
-    </div>
+    <Container>
+      <Segment raised>
+        <div id="card" className="flip-container animated flashcard flipInx">
+          <Card id="card-front" className="flip-card-front on">
+            <Card.Content>
+              <Card.Header>
+                Questiondfghjklm-ghgvjbknlm,gfhjcbklvd
+              </Card.Header>
+            </Card.Content>
+          </Card>
+          <Card id="card-back" className="flip-card-back off">
+            <Card.Content>
+              <Card.Header>
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+              </Card.Header>
+            </Card.Content>
+          </Card>
+        </div>
+        <div className="nav-buttons">
+          <Button>
+            <Icon name="check" />
+          </Button>
+          <Button>
+            <Icon name="close icon" />
+          </Button>
+        </div>
+      </Segment>
+    </Container>
   );
 };
 
