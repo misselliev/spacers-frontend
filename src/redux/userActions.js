1c676960eca1df18952e8f13de9518094144447a
Elizabeth Villalejos <nekobanpaia@gmail.com>
2020-08-24 09:28:47 -0700

    Adding userActions/Reducer

---
+++ src/redux/userActions.js
@@ -0,0 +1,45 @@
+import axios from 'axios';
+
+const setUser = userObj => ({
+  type: 'LOGIN_USER',
+  payload: userObj,
+});
+
+const clearUser = () => ({
+  type: 'CLEAR_USER',
+});
+
+const registerUser = userObj => ({
+  type: 'NEW_USER',
+  payload: userObj,
+});
+
+const loginUser = userObj => dispatch => {
+  axios.post('https://localhost:3001', userObj).then(res => {
+    dispatch(setUser(res.data.data));
+    localStorage.setItem('user', JSON.stringify(res.headers));
+  }).catch(error => {
+    throw (error);
+  });
+};
+
+const logoutUser = () => dispatch => {
+  const headers = JSON.parse(localStorage.user);
+  axios.delete('https://localhost:3001', { headers });
+  dispatch(clearUser());
+  localStorage.clear();
+};
+
+const newUser = userObj => dispatch => {
+  axios.post('https://localhost:3001', userObj).then(res => {
+    dispatch(registerUser(res.data.data));
+  }).catch(error => {
+    throw (error);
+  });
+};
+
+export default {
+  loginUser,
+  logoutUser,
+  newUser,
+};
