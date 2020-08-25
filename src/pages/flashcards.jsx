9448d24340a5cef0775e8e35751b6ca104af53f0
FelipeEnne <felipeenne@gmail.com>
2020-08-25 18:59:22 -0300

    fix bug

--- src/pages/flashcards.jsx
+++ src/pages/flashcards.jsx
@@ -1,32 +1,33 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector, shallowEqual } from 'react-redux';
+import React /* , { useEffect } */ from 'react';
+// import { useDispatch, useSelector, shallowEqual } from 'react-redux';
 import {
   Segment, Grid, Container,
 } from 'semantic-ui-react';
-import flashcardActions from '../redux/flashcardActions';
+// import flashcardActions from '../redux/flashcardActions';
 import Flashcard from '../components/flashcard';
 
 const NewFlashcard = () => {
-  const flashcards = useSelector(state => state.flashcards, shallowEqual) || [];
+  // const flashcards = useSelector(state => state.flashcards, shallowEqual) || [];
   // const user = useSelector(state => state.user.currentUser) || {};
   // const userToken = user.token;
-  const dispatch = useDispatch();
+  // const dispatch = useDispatch();
 
-  useEffect(() => {
-    dispatch(flashcardActions.fetchCards());
-  }, [dispatch]);
+  // useEffect(() => {
+  // dispatch(flashcardActions.fetchCards());
+  // }, [dispatch]);
 
-  // const flashcards = [
-  //   { id: '2', question: 'who is a good boy', answer: 'all dogs are good boys' },
-  //   { id: '3', question: 'who is a good boy', answer: 'all dogs are good boys' },
-  // ];
+  const flashcards = [
+    { id: '2', question: 'who is a good boy', answer: 'all dogs are good boys' },
+    { id: '3', question: 'who is a good boy', answer: 'all dogs are good boys' },
+  ];
+
   return (
     <Container>
       <Grid textAlign="center" className="middle aligned wall login">
         <Grid.Column className="flashcard-column">
           <Segment raised>
             {flashcards.map(({ id, question, answer }) => (
-              <Flashcard key={id} question={question} answer={answer} />
+              <Flashcard key={id} id={id} question={question} answer={answer} />
             ))}
           </Segment>
         </Grid.Column>
